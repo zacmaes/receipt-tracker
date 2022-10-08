015d41f390da00916064ca235764fe0b4f7424aa
zacmaes <zachary.maes@gmail.com>
2022-10-08 16:19:21 -0400

    total price function logic is working, added pseudocode city count function

--- app.js
+++ app.js
@@ -2,6 +2,32 @@ console.log("It's alive!")
 
 const data = require('./test-data.json')
 
-console.log(data.receipts[1].totalPrice)
+const receipts = data.receipts  // the receipts array
 
+let addTotalPrices = () => {
+    // loops through json and adds the total prices to return the total expenditure.
 
+    let runningTotal = 0
+    console.log(runningTotal)
+
+    for (let i = 0; i < receipts.length; i++) {
+        runningTotal += receipts[i].totalPrice
+        console.log("------------------------------------------------------------------------")        
+        console.log(`Company name: ${receipts[i].company}, add: ${receipts[i].totalPrice} ...`)
+        console.log(`New Total: ${runningTotal}`)
+        console.log("------------------------------------------------------------------------")        
+    }
+}
+
+let cityCount = () => {
+    let cities = {}
+
+    // for every index in receipts array
+    //      if city not in cities object
+    //          add {city : 1} to cities object
+    //      else add 1 to the value of this city instance key in cities object
+
+    // print the total days spent in each city based upon the receipts
+}
+
+addTotalPrices()
