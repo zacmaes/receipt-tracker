12d315d6b737edc57fd23aa0d14a4578c551f7b5
zacmaes <zachary.maes@gmail.com>
2022-10-09 09:26:02 -0400

    added some instruction constants

--- app.js
+++ app.js
@@ -4,6 +4,27 @@ const data = require('./test-data.json')    // Require the json receipt data
 
 const receipts = data.receipts  // assign the receipts array to a variable for ease of use
 
+// Instructions Code
+const instructions1 = "Hello there! My name is Zac."
+const instructions2 = "For the last year, my wife has been travel nursing, and we have been travelling around the United States while I complete my degree."
+const instructions3 = "The United States Internal Revenue Service (IRS) requires that we reside within our 'tax home' for 30 days per year."
+const instructions4 = "We have been keeping receipts from various locations for our records."
+const instructions5 = "This program saves receipt data in json format, runs calculations on the receipt data, and returns that information in the console."
+
+const linebreak     = "--------------------------------------------------------------------------------------------------------"
+const 
+
+const instructions5 = ""
+
+
+
+
+
+
+
+
+
+
 let addTotalPrices = () => {
     // Function: 
     // -assigns a runningTotal count to 0
